b41862b8909f791feb5692c49db2d67dfe50c4ef
iamlixiaoyi80 <lixiaoyi80@users.noreply.github.com>
2026-03-11 08:44:19 +0800

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
-import { fileURLToPath, dirname } from 'url'
-
-import { readFileSync } from 'fs'
-
-import { homedir } from 'os'
-import type { AddressInfo } from 'net';
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: './',
   build: {
     outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-    '@': fileURLToPath(dirname(fileURLToPath(import.meta.url)))
-  }
-  },
-  server: {
-    port: 5173,
-    host: true,
-    strictPort: true,
   },
 })
